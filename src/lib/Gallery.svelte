39db2132de7e0c4ee959c8dabe74d3e98fbf4616
Jean-Frédéric Faust <4367222+jffaust@users.noreply.github.com>
2025-02-28 16:16:30 -0500

    allow image component prop

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -2,7 +2,7 @@
   import Img from './Img.svelte';
   import layout from './layout';
 
-  let { images = [], rowHeight = 220, gutter = 8, children } = $props();
+  let { images = [], rowHeight = 220, gutter = 8, children, ImageComponent = Img } = $props();
   let width = $state(0);
   let scaledImages = $derived(layout({
     images,
@@ -61,7 +61,7 @@
         {#if children}
           {@render children(index, image)}
         {:else}
-          <Img {...image} />
+          <ImageComponent {...image} />
         {/if}
       </div>
     {/each}
